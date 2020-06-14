cf843e798fc48736f74fb9fd4a322d52f5cf1fc8
David Kohler <dako.davidkohler@gmail.com>
2020-06-14 12:26:32 -0500

    [add] added user input for dimensions

--- src/ElementaryCAApp.java
+++ src/ElementaryCAApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ElementaryCAApp {
 
     /**
@@ -18,11 +20,40 @@ public class ElementaryCAApp {
     }
 
     /**
+     * Gets dimensions of window from user
+     * @param sc    Scanner object for user input
+     * @param dim   String indicating which dimension currently parsing
+     * @return      int input of chosen dimension
+     */
+    public static int inputDimensions(Scanner sc, String dim) {
+        int num;
+        do {
+            System.out.printf("\nPlease Enter Window %s (must not be more than 1280): ", dim);
+            while (!sc.hasNextInt()) {
+                System.out.println("Invalid response!");
+                System.out.printf("\nPlease Enter Window %s (must not be more than 1280): ", dim);
+                sc.next();
+            }
+            num = sc.nextInt();
+            if (num <= 0 || num > 1280) {
+                System.out.println("Invalid response!");
+            }
+        } while (num <= 0 || num > 1280);
+        System.out.printf("%s confirmed!\n", dim);
+        return num;
+    }
+
+    /**
      * Main method
      * @param args The runtime args.
      */
     public static void main(final String... args) {
-        // TODO: make this customizable
-        new ElementaryCAApp(400, 200);
+        int width;
+        int height;
+        Scanner sc = new Scanner(System.in);
+        width = inputDimensions(sc, "Width");
+        height = inputDimensions(sc, "Height");
+
+        new ElementaryCAApp(width, height);
     }
 }
